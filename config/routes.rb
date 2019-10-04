cb30db1025bb26d48eaad617cd5f0f270c37ec96
Rodpoolazo <rodpoolazo@gmail.com>
2019-10-04 19:14:29 -0300

    add table auctionlisting , jquery, datatables, controlador devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-    root to: 'home#index'
+    get 'admins/update_role/:user_id/:role_id', to: 'admins#update_role', as: 'admins_update_role'
+    get 'admins/index'
 
+      devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
 
+      resources :auctionlistings
 
+      post 'auctionnotice/:id', to: 'auctionnotices#action_selection', as: 'action_selection'
+      get 'auctionnotices/pending'
+      get 'auctionnotices/selected'
+      get 'auctionnotices/rejected'
 
+      resources :auctionnotices do
+        resources :notice_selections
+      end
 
+      resources :realties
 
+      resources :communes
+      resources :provinces
+      resources :regions
 
+        resources :notice_selections
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      root to: 'auctionlistings#index'
 
 end
